d2757e7e64567cd315e3d3cacbd86728661caa49
lokesh-dc <lokesh.cdewanand@gmail.com>
2023-07-04 22:00:48 +0530

    Get more button

--- components/buttons/GetMore.tsx
+++ components/buttons/GetMore.tsx
@@ -7,7 +7,7 @@ const getMoreButton: React.FC<props> = ({ clickevent }) => {
 	return (
 		<button
 			className="w-full flex justify-center mt-7 text-center bg-slate-400 p-2"
-			onClick={clickevent}
+			onClick={() => clickevent()}
 		>
 			SEE MORE
 		</button>
