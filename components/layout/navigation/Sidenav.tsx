b74a45760096b9628875db45a9cc878dab692066
lokesh-dc <lokesh.cdewanand@gmail.com>
2023-10-02 13:44:36 +0530

    side na

--- components/layout/navigation/Sidenav.tsx
+++ components/layout/navigation/Sidenav.tsx
@@ -41,8 +41,12 @@ const SideNav: React.FC<props> = ({ status, toggleStatus }): ReactElement => {
 							<Link href={"/"}>ON_SCREEN</Link>
 						</h2>
 					</div>
-					{sideNavData.map((item) => (
-						<li className="relative" onClick={() => toggleStatus(!status)}>
+					{sideNavData.map((item, index) => (
+						<li
+							key={index}
+							className="relative"
+							onClick={() => toggleStatus(!status)}
+						>
 							<Link
 								href={item.href}
 								className="flex h-12 cursor-pointer items-center truncate rounded-[5px] px-6 py-4 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
@@ -53,11 +57,9 @@ const SideNav: React.FC<props> = ({ status, toggleStatus }): ReactElement => {
 						</li>
 					))}
 				</ul>
-            </nav>
-            
-            <div>
-                X
-            </div>
+			</nav>
+
+			<div>X</div>
 		</div>
 	);
 };
