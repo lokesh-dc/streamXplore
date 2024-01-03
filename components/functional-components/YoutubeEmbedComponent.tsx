ea6e0d2fe332f266139b5763f1b471a0eff7f425
lokesh-dc <lokesh.cdewanand@gmail.com>
2024-01-04 01:34:01 +0530

    Fix : Modal video play

--- components/functional-components/YoutubeEmbedComponent.tsx
+++ components/functional-components/YoutubeEmbedComponent.tsx
@@ -19,6 +19,27 @@ const YoutubeEmbedComponent: React.FC<props> = ({
 		setVideoIdState(videoId);
 	}, [videoId]);
 
+	useEffect(() => {
+		const button = document.querySelector(".lty-playbtn");
+
+		if (!button) return;
+
+		function createObserver() {
+			let observer;
+
+			let options = {
+				rootMargin: "-50%",
+				threshold: 1,
+			};
+
+			// @ts-ignore
+			observer = new IntersectionObserver(() => button.click(), options);
+			// @ts-ignore
+			observer.observe(button);
+		}
+		return createObserver();
+	}, [videoId]);
+
 	return (
 		<div className={`${classes}`}>
 			<LiteYouTubeEmbed id={videoIdState} title={title} />
